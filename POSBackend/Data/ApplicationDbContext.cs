efcd43bf0d65e873e4c00e349781068a5341a240
KusalDemo <kusaldemo2022@gmail.com>
2024-09-20 11:32:16 +0530

    Implemented ApplicationDbContext
    - This class serves as the primary context for interacting with the database using Entity Framework Core. It includes DbSets for Customer, Item, OrderItem, and PlaceOrder, facilitating CRUD operations and managing relationships between entities in the POS system.

---
+++ POSBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POSBackend.Models.Entities;
+
+namespace POSBackend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<PlaceOrder> PlaceOrders  { get; set; }
+
+    }
+}
