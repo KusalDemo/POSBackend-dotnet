137d8e6b38b3d8414ee8f481fbc8b1b269a4fbbb
KusalDemo <kusaldemo2022@gmail.com>
2024-09-20 12:59:06 +0530

    Created CustomerController. (base)
    - Implemented CustomerController to manage customer-related operations.
    - Utilized Dependency Injection to inject ApplicationDbContext, allowing access to the database context within the controller.
    - Added the GetAllCustomers method, which retrieves all customer records from the database and returns them as a JSON response.
    - This sets the groundwork for further customer management functionalities in the API.

---
+++ POSBackend/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using POSBackend.Data;
+
+namespace POSBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        //Constructor through Injection (Database Context)
+        public CustomerController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            var  allEmployees=dbContext.Customers.ToList();
+            return Ok(allEmployees);
+        }
+    }
+}
